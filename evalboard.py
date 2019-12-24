1369368780fd4ca9f0214d26baa9a09f3093973e
Joseph vantassel <jvantassel@utexas.edu>
2019-12-23 19:35:56 -0600

    :white_check_mark: Update and pass evalboard tests

--- evalboard.py
+++ evalboard.py
@@ -1,5 +1,4 @@
-"""
-Determine probability distribution of current board by:
+"""Determine probability distribution of current board by:
 1) Check if the current board (or any equivalent) has been seen before.
 2) If so, load previous probability.
 3) If not, make diffuse assumption.
@@ -11,7 +10,9 @@ import unittest
 from transform import board_transform
 
 
-def viewboard(state, boards_played, p1=1, p2=2):
+def recall_board(state, boards_played, p1=1, p2=2):
+    """Determine if board (or equvialent) has been seen before."""
+
     # Check if the board (or eqivalent) has been seen before
     for trans_number in range(8):
         trans_state = board_transform(state, trans_number)
@@ -21,10 +22,11 @@ def viewboard(state, boards_played, p1=1, p2=2):
         if key in boards_played:
             return (True, key, trans_number)
 
-    return (False, key, trans_number)
+    return (False, nd3_to_tuple(state), 0)
 
 
-def diffuse(state, p1=1, p2=2):
+def diffuse_utility(state, p1=1, p2=2):
+    """Use diffuse assumption to assign utility."""
     # Set all values not equal to p1 or p2 to diffuse utility
     row, col = np.where((state != p1) & (state != p2))
     utility = np.ones((3, 3))*-np.inf
@@ -35,16 +37,23 @@ def diffuse(state, p1=1, p2=2):
 
 
 def nd3_to_tuple(array):
+    """Take ndarray and convert to tuples (i.e., immutable type)"""
     return (tuple(array[0]), tuple(array[1]), tuple(array[2]))
 
+
 def tuple_to_nd3(key):
+    """Transform key (i.e., immutable type) back to ndarray."""
     return np.array(key)
 
+
 class Test_EvalBoard(unittest.TestCase):
+    def assertArrayEqual(self, array1, array2):
+        self.assertListEqual(array1.tolist(), array2.tolist())
+
     def test_diffuse(self):
         board = np.array([[0, 0, 0], [0, 0, 0], [0, 0, 0]])
         known_util = np.zeros((3, 3))
-        self.assertListEqual(diffuse(board, p1=1, p2=2).tolist(),
+        self.assertListEqual(diffuse_utility(board, p1=1, p2=2).tolist(),
                              known_util.tolist(),
                              )
 
@@ -53,54 +62,54 @@ class Test_EvalBoard(unittest.TestCase):
                                [-np.inf, -np.inf, 0],
                                [0, 0, 0]]
                               )
-        test_util = diffuse(board, p1=1, p2=2)
-        for known, test in zip(known_util.tolist(), test_util.tolist()):
-            self.assertListEqual(known, test)
+        test_util = diffuse_utility(board, p1=1, p2=2)
+        self.assertArrayEqual(known_util, test_util)
 
     def test_tuple_to_nd3(self):
-        expected = np.array([[1,2,3],[4,5,6],[7,8,9]])
+        expected = np.array([[1, 2, 3], [4, 5, 6], [7, 8, 9]])
         tpl = nd3_to_tuple(expected)
         found = tuple_to_nd3(tpl)
-        self.assertListEqual(expected.tolist(), found.tolist())
+        self.assertArrayEqual(expected, found)
 
-    # def test_evalboard_initial(self):
-    #     board = np.array([[0, 0, 0], [0, 0, 0], [0, 0, 0]])
-    #     known_util = np.zeros((3, 3))
-    #     known_key = nd3_to_tuple(board)
-    #     a, b, c, d = evalboard(board, {}, p1=1, p2=2)
-    #     self.assertListEqual(a.tolist(), known_util.tolist())
-    #     self.assertTupleEqual(b, known_key)
-    #     self.assertEqual(c, 7)
-    #     self.assertTrue(d)
+    def test_recall_board(self):
+        # Initial Board
+        board = np.array([[0, 0, 0], [0, 0, 0], [0, 0, 0]])
+        seen_before, key, trans_number = recall_board(board, {}, p1=1, p2=2)
 
-    # def test_evalboard_second(self):
-    #     board = np.array([[0, 2, 0], [2, 1, 2], [1, 1, 0]])
-    #     util = np.array([[1, -np.inf, 3],
-    #                      [-np.inf, -np.inf, -np.inf],
-    #                      [-np.inf, -np.inf, 9]])
-    #     key = nd3_to_tuple(board)
-    #     a, b, c = evalboard(np.flip(board, 0), {key: util}, p1=1, p2=2)
-    #     self.assertListEqual(a.tolist(), util.tolist())
-    #     self.assertTupleEqual(b, key)
-    #     self.assertEqual(c, 5)
+        self.assertFalse(seen_before)
+        self.assertArrayEqual(board, tuple_to_nd3(key))
+        self.assertEqual(0, trans_number)
 
-    # def test_transform(self):
-    #     board_1 = np.array([[1, 1, 0], [0, 0, 0], [0, 0, 0]])
-    #     a1, b1, c1, d1 = evalboard(board_1, {}, p1=1, p2=2)
-    #     util = np.array([[-np.inf, -np.inf, 0],
-    #                      [0, 0, 0],
-    #                      [0, 0, 0]])
-    #     self.assertListEqual(a1.tolist(), util.tolist())
-    #     key = nd3_to_tuple(board_1)
-    #     self.assertTupleEqual(b1, key)
-    #     self.assertEqual(c1, 7)
-    #     self.assertTrue(d1)
+        # Later move
+        board = np.array([[0, 2, 0], [2, 1, 2], [1, 1, 0]])
+        util = np.array([[1, -np.inf, 3],
+                         [-np.inf, -np.inf, -np.inf],
+                         [-np.inf, -np.inf, 9]])
+        key = nd3_to_tuple(board)
+        seen_before, received_key, trans_number = recall_board(np.flip(board, 0),
+                                                               {key: util},
+                                                               p1=1, p2=2)
+        self.assertTrue(seen_before)
+        self.assertArrayEqual(board, tuple_to_nd3(received_key))
+        self.assertEqual(6, trans_number)
 
-    #     board_2 = np.array([[1, 0, 0], [1, 0, 0], [0, 0, 0]])
-    #     a2, b2, c2 = evalboard(board_2, {b1: a1}, p1=1, p2=2)
-    #     self.assertListEqual(a2.tolist(), a1.tolist())
-    #     self.assertTupleEqual(b2, b1)
-    #     self.assertEqual(c2, 2)
+        # Later move
+        board = np.array([[1, 1, 0], [0, 0, 0], [0, 0, 0]])
+        seen_before, key, trans_number = recall_board(board, {}, p1=1, p2=2)
+        util = np.array([[-np.inf, -np.inf, 0],
+                         [0, 0, 0],
+                         [0, 0, 0]])
+        
+        self.assertFalse(seen_before)
+        self.assertArrayEqual(board, tuple_to_nd3(key))
+        self.assertEqual(0, trans_number)
+        
+        # Later move
+        board = np.array([[1, 0, 0], [1, 0, 2], [0, 0, 2]])
+        key = nd3_to_tuple(np.transpose(board))
+        seen_before, key, trans_number = recall_board(board, {key:diffuse_utility(board)}, p1=1, p2=2)
+        self.assertTrue(seen_before)
+        self.assertEqual(3, trans_number)
 
 
 if __name__ == "__main__":
